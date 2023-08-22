3bca5b01ac8126d5a1152ae35959ccb9508767dd
Cosmic-Abyss <roccobald@gmail.com>
2023-08-22 15:26:49 -0400

    transferring from gitlab to personal github

---
+++ vue/src/components/gbaEmulator.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <div class="gba-container">
+    <iframe class="gbaEmulator" src="/gbajs2-master/index.html" scrolling="no"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    document.body.classList.add('no-scroll');
+  },
+  beforeDestroy() {
+    document.body.classList.remove('no-scroll');
+  }
+};
+</script>
+
+<style>
+.gba-container {
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(to bottom, #FFA500, #FF4500);
+  overflow: hidden;  /* Hide any overflowing content */
+  position: relative;  /* To position the iframe inside it */
+}
+
+.gbaEmulator {
+  position: absolute;
+  top: -10%;  /* This pushes the top part of the iframe content out of view */
+  left: 0;
+  width: 100%;
+  height: 110%;  /* This extends the iframe below the container */
+  border: none;
+  overflow: hidden;
+}
+body.no-scroll {
+  overflow-y: hidden;
+}
+
+</style>
